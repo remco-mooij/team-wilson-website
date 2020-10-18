f91b6922b2233523a45782e049e6208f2473957a
unknown <remcomooij@gmail.com>
2020-10-17 21:18:07 -0400

    added register form, validators and flash message

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '389c4704e2873f77fa5ecad8fe3e4046e1925c4d'
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+  return render_template("home.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    flash(f'Account created for {form.username.data}!', 'success')
+    return redirect(url_for('home'))
+  return render_template("register.html", form=form)
+
+@app.route("/login")
+def login():
+  form = LoginForm()
+  return render_template("login.html", form=form)
 
 @app.route("/petty_cash")
 def petty_cash():
-    return render_template("petty_cash.html")
+  return render_template("petty_cash.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
