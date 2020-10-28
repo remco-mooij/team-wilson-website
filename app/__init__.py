bd6f74df9c43d6a160572e80ee4eec5363281445
unknown <remcomooij@gmail.com>
2020-10-28 18:28:39 -0400

    added login, register and account pages and functionality

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '389c4704e2873f77fa5ecad8fe3e4046e1925c4d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///team_wilson.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from app import routes
